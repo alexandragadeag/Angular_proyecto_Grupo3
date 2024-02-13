000f7a53002e86f32a91f141ad4950d09e202df5
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-02-13 14:04:02 +0100

    Update user-list component.ts

--- frontend/src/app/user-list/user-list.component.ts
+++ frontend/src/app/user-list/user-list.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { User } from '../interfaces/user.model';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css'
 })
@@ -15,7 +15,7 @@ export class UserListComponent {
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-   this.http.get<User[]>('http://localhost:300/users')
+   this.http.get<User[]>('http://localhost:3000/users')
    .subscribe(users => this.users = users);
   }
 
