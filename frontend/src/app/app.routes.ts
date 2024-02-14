23eb83751370ae705f45a35c8316a86284407bad
Marian Gómez Blanco <marianattack@gmail.com>
2024-02-14 12:28:55 +0100

    error en routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { UserListComponent } from './user-list/user-list.component';}
+import { UserListComponent } from './user-list/user-list.component';
 
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'users', component: UserListComponent }
+export const route: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'users', component: UserListComponent }
 ];
-export const routes: Routes = [];
