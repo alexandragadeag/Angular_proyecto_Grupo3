2e2578ace16347f8d7dc234f5464c65847eb91d6
Marian Gómez Blanco <marianattack@gmail.com>
2024-02-14 12:21:55 +0100

    app routes modificado

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserListComponent } from './user-list/user-list.component';}
 
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UserListComponent }
+];
 export const routes: Routes = [];
