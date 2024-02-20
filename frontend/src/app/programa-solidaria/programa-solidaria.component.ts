b5c7f679724467ee5b3eed018d56ccc09bd5bb3c
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-02-20 11:02:11 +0100

    Revert "Limpieza de componentes"
    This reverts commit 84cf9847e897e62bf93159851061fc626f9655ec.

---
+++ frontend/src/app/programa-solidaria/programa-solidaria.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-programa-solidaria',
+  standalone: true,
+  imports: [],
+  templateUrl: './programa-solidaria.component.html',
+  styleUrl: './programa-solidaria.component.css'
+})
+export class ProgramaSolidariaComponent {
+
+}
---
+++ frontend/src/app/viviendas/viviendas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-viviendas',
+  standalone: true,
+  imports: [],
+  templateUrl: './viviendas.component.html',
+  styleUrl: './viviendas.component.css'
+})
+export class ViviendasComponent {
+
+}
