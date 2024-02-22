29b927721944b1cdd9a5911645701083e6d23a79
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-02-22 20:27:19 +0100

    Angular Carousel

---
+++ frontend/src/app/home/Style.js
@@ -0,0 +1,9 @@
+// Initialization for ES Users
+import { Carousel, initMDB } from "mdb-ui-kit";
+
+initMDB({ Carousel });
+
+// Initialization for ES Users
+import { Collapse, initMDB } from 'mdb-ui-kit';
+
+initMDB({ Collapse });
