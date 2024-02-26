9d59dfc327d79ed1e13a8010b505d5fa6021b0b0
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-02-26 13:36:17 +0100

    Angular Navbar ajustes

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { UserListComponent } from "./user-list/user-list.component";
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, UserListComponent, RouterLink]
+    imports: [NgbDropdownModule,RouterOutlet, UserListComponent, RouterLink]
 })
 export class AppComponent {
   title = 'frontend';
+  collapsed = true;
 }
