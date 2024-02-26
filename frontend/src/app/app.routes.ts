e8c8d0ef0786c7bc1174972f488da513219b7d25
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-02-26 14:00:07 +0100

    merge

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UserListComponent } from './user-list/user-list.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 export const route: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'users', component: UserListComponent }
+    { path: 'users', component: UserListComponent },
+    { path: 'users/:id', component: UserDetailComponent },
     
 ];
