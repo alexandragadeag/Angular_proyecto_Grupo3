afa28e3d48e15e329a96fe1ad050f355a9ad0868
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-02-26 12:12:28 +0100

    Angular style button css

--- frontend/src/app/home/Style.js
+++
@@ -1,9 +0,0 @@
-// Initialization for ES Users
-import { Carousel, initMDB } from "mdb-ui-kit";
-
-initMDB({ Carousel });
-
-// Initialization for ES Users
-import { Collapse, initMDB } from 'mdb-ui-kit';
-
-initMDB({ Collapse });
