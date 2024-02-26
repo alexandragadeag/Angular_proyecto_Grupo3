283cf5f5823e7106f72d005ed710d25a7397ade0
Marian Gómez Blanco <marianattack@gmail.com>
2024-02-26 13:48:23 +0100

    User detail, app routes y font ok

--- frontend/src/app/user-detail/user-detail.component.ts
+++ frontend/src/app/user-detail/user-detail.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../interfaces/user.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.css'
 })
-export class UserDetailComponent {
+export class UserDetailComponent implements OnInit {
 
+  user: User | undefined;
+
+  constructor(private activatedRoute: ActivatedRoute,
+        private router: Router) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      this.getUserDetails(params['id']);
+    });
+  }
+
+  getUserDetails(id: string): void {
+    
+  }
 }
