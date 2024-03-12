a35c428684a4cdb1feb33fa9af00639fee3df806
Marian Gómez Blanco <marianattack@gmail.com>
2024-03-12 11:36:48 +0100

    config appmodule.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost', 
+      port: 3306,
+      username: 'root',
+      password: 'admin1234',
+      database: 'nest', // crear esta base de datos en MySQL primero
+      entities: [],
+      synchronize: true, // generar tablas en base de datos
+      logging: true
+    }),
+    TypeOrmModule.forFeature([]) // Esto permite acceder a Repository
+    
+  ],
+  controllers: [AppController, ],
   providers: [AppService],
 })
 export class AppModule {}
