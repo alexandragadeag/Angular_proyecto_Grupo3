4e90686bf8f67cad46ea9a58c22c7210d07d2295
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-03-20 20:09:45 +0100

    Angular Frontend creación de componentes contract-list y contract-detail

---
+++ frontend/src/app/contract-detail/contract-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contract-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './contract-detail.component.html',
+  styleUrl: './contract-detail.component.css'
+})
+export class ContractDetailComponent {
+
+}
