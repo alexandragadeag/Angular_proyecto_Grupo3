67ae5c392ed7012fba3b7b076350093c4bb91230
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-03-21 20:05:03 +0100

    Nest backend contract.controller.ts InjectRepository y @Get  findAll

--- backend/src/contract/contract.controller.ts
+++ backend/src/contract/contract.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Contract } from './contract.model';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Controller('contract')
-export class ContractController {}
+export class ContractController {
+    constructor(
+        @InjectRepository(Contract) private contractRepository: Repository<Contract>
+    ) { } 
+
+    @Get()
+    findAll() {
+        return this.contractRepository.find(); // Equivalente al SELECT * FROM
+    }
+}
