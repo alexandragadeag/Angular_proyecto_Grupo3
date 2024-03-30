e4f7a36e778d6258c6cf5c58552ca961dccf6241
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-03-30 23:17:17 +0100

    Angular Frontend product-list.component.ts

--- frontend/src/app/product-list/product-list.component.ts
+++ frontend/src/app/product-list/product-list.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Product } from '../interfaces/product.model';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, RouterLink],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
+    
+  products: Product[] = [];
+
+  constructor(private httpClient: HttpClient) {}
+   
+  
+  ngOnInit(): void {
+       this.httpClient.get<Product[]>('http://localhost:3000/product')
+       .subscribe(products => this.products = products);
+   }
+
 
 }
