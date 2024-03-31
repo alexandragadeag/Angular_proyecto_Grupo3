0ba8e2ca45731173b57a34cefb555dc4bf446cd4
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-03-31 14:04:38 +0200

    Frontend product-detail HTML y TS

--- frontend/src/app/product-detail/product-detail.component.ts
+++ frontend/src/app/product-detail/product-detail.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Product } from '../interfaces/product.model';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, RouterLink],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit{
+
+  product: Product | undefined;
+  
+
+  constructor(private httpClient: HttpClient,
+    private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      const id = params['id'];
+      if(!id) {
+        return; 
+      }
+
+      // traer products
+      this.httpClient.get<Product>('http://localhost:3000/product/' + id)
+      .subscribe(product => this.product = product);
+
+    });
+  }
+
 
 }
