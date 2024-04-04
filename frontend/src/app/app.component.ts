088dc22e467fdcd148e8cddb7beb72191e3d8ef7
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-04-04 11:17:27 +0200

    FullStack navbar isLoggedIn y mostrar botones login o logout

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { AuthenticationService } from './authentication/authentication.service';
 @Component({
     selector: 'app-root',
     standalone: true,
@@ -11,4 +12,11 @@ import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 export class AppComponent {
   title = 'frontend';
   collapsed = true;
+
+  isLoggedIn = false;
+
+  constructor(private authService: AuthenticationService) {
+    // Esto permite que el componente Navbar se entere de que ha ocurrido un login exitoso
+    this.authService.isLoggedIn.subscribe(isLoggedIn => this.isLoggedIn = isLoggedIn);
+  }
 }
