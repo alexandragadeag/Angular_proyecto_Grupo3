af0a1cdf916c885941f53ff23f053a561ee6b565
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-04-08 13:30:24 +0200

    Angular Frontend isAdmin para comprobar rol de usuario AuthService

--- frontend/src/app/authentication/authentication.service.ts
+++ frontend/src/app/authentication/authentication.service.ts
@@ -14,18 +14,17 @@ export class AuthenticationService {
   // Notifica a quien se haya suscrito a este booleano de que ha ocurrido un login
   isLoggedIn = new BehaviorSubject<boolean>(this.hasToken());
   userEmail = new BehaviorSubject<string>(this.getCurrentEmail());
-
+  isAdmin = new BehaviorSubject<boolean>(this.getIsAdmin());
   constructor() { }
-
   hasToken(): boolean {
     return localStorage.getItem("jwt_token") !== null;
   }
-
   handleLogin(token: string) {
     // guardar el token en el almacenamiento del navegador
     localStorage.setItem("jwt_token", token);
     this.isLoggedIn.next(true);
     this.userEmail.next(this.getCurrentEmail());
+    this.isAdmin.next(this.getIsAdmin());
   }
   getCurrentEmail() {
     // decodificar el token y extraer la información que tiene dentro
@@ -35,10 +34,18 @@ export class AuthenticationService {
     const decodedToken = jwtDecode(token) as DecodedToken;
     return decodedToken.email;
   }
+  getIsAdmin() {
+    const token = localStorage.getItem("jwt_token");
+    if(!token) return false;
+
+    const decodedToken = jwtDecode(token) as DecodedToken;
+    return decodedToken.role === 'admin'; // true o false
+  }
   logout() {
     localStorage.removeItem("jwt_token");
     this.isLoggedIn.next(false);
     this.userEmail.next('');
+    this.isAdmin.next(false);
   }
 
 }
