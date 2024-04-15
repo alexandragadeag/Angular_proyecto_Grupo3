4f09495eb079e0add0a3874a3e7406b8a1e7b932
alexandragadeag <149069703+alexandragadeag@users.noreply.github.com>
2024-04-15 12:10:00 +0200

    Frontend arreglado botón registro

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Login } from '../interfaces/login.model';
 import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AuthenticationService } from '../authentication/authentication.service';
 import { Token } from '../authentication/token.dto';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
